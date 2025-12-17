7b8aa6c66645d44c3403d249fab3fee26f0fcc71
Rian-azevedo <riandeoliveiraazevedo63@gmail.com>
2025-12-17 07:39:40 -0300

    terminado a area de cursos

--- gestao_alunos/cursos/models.py
+++ gestao_alunos/cursos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from professores.models import Professor
 
+class Curso(models.Model):
+    nome = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=20, unique=True)
+    carga_horaria = models.PositiveIntegerField()
+    Professor = models.ForeignKey(
+        Professor,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    
+    
+    def __str__(Self):
+        return f"{self.nome} ({self.codigo})"
 # Create your models here.
+
