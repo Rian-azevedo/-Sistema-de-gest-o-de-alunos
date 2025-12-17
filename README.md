3a8e59d00e9616b950ae1cf129306f3ae4b86014
Rian-azevedo <riandeoliveiraazevedo63@gmail.com>
2025-12-16 23:45:48 -0300

    commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# -Sistema-de-gest-o-de-alunos
 sistema que facilita o controle de informações de alunos e turmas.
---
+++ gestao_alunos/alunos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Alunos(models.Model):
+    matricula = models.CharField(max_length=20, primary_key=True)
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    data_nascimento = models.DateField()
+    
+    def __str__(self):
+        return self.nome
